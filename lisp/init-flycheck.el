a9bed76a756a8e125b8eb8e37f2b3bc3c59bd587
ßingen <bingen@voltanet.io>
2017-08-01 12:09:03 +0200

    Add flycheck hooks configuration

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -4,6 +4,7 @@
 
 (require-package 'flycheck)
 
-;(add-hook 'after-init-hook #'global-flycheck-mode)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(add-hook 'c++-mode-hook (lambda () (setq flycheck-gcc-language-standard "c++11")))
 
 (provide 'init-flycheck)
