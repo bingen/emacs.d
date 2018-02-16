97496ea702b5e6edac02b0cafd5ea6cc1d619c58
ßingen <bingentxu@gmail.com>
2018-02-16 14:19:34 +0100

    Set default font size

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -7,5 +7,9 @@
 (require 'linum)
 (global-linum-mode 1)
 
+;; Set default font size
+;; https://stackoverflow.com/a/296316/1937418
+(set-face-attribute 'default nil :height 80)
+
 (provide 'init-local)
 
