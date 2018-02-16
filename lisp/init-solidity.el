d0f0b09192dc7a6e544fa78750fe8ec9470d3673
ßingen <bingentxu@gmail.com>
2018-02-16 14:19:05 +0100

    Set Solidity indentation to 4

--- lisp/init-solidity.el
+++ lisp/init-solidity.el
@@ -4,5 +4,13 @@
 
 (require-package 'solidity-mode)
 
+;; https://emacs.stackexchange.com/a/17565/12560
+(defun solidity-custom-settings ()
+  (setq tab-width 4)
+  (setq c-basic-offset 4))
+
+(add-hook 'solidity-mode-hook 'solidity-custom-settings)
+
 (provide 'init-solidity)
-;;;
+
+;;; init-solidity.el ends here
